880d6cab892adb0a926461ed3efbab9cd79ddf71
jesscococ09 <jesstorrealba@gmail.com>
2025-09-28 13:32:20 -0700

    added comments

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -5,7 +5,8 @@ import java.util.Map;
 import java.util.Objects;
 
 /**
- * [Brief one-sentence description of what this class does.]
+ * manages a collection of books organized by subject and shelf number, tracking inventory counts
+ * and allows us to add, remove, and list books.
  *
  * @author Jessika Torrealba
  * @version 0.1.0
@@ -68,12 +69,24 @@ public class Shelf {
     public String toString() {
         return shelfNumber+" : "+ subject;
     }
+    /*
+    If book is null or there is not any copies of the book in the self
+    return -1
+    else
+    @returns the number of copies in the self.
+     */
     public int getBookCount(Book book) {
         if(book==null || !books.containsKey(book)){
             return -1;
         }
         return books.get(book);
     }
+    /*
+    if book is null return error code, if subject not found on self return error
+    if the count is -1, book is added and count is changed to 1
+    else it adds book, and increases count
+    @returns Code Success and prints a success message
+     */
     public Code addBook(Book book) {
         if(book==null){
             return Code.UNKNOWN_ERROR;
@@ -90,6 +103,12 @@ public class Shelf {
         System.out.printf("%s %s %s",book.toString()," added to shelf " ,this.toString());
         return Code.SUCCESS;
     }
+    /*
+    if book is null return error code, if book is not on self return error
+    if the count is 0, return error and a message
+    else it removes book, and decrease count
+    @returns Code Success and prints a success message
+     */
     public Code removeBook(Book book) {
         if(book==null){
             return Code.UNKNOWN_ERROR;
@@ -106,6 +125,10 @@ public class Shelf {
         System.out.printf("%s %s %s",book.getTitle(),"successfully removed from shelf ",subject);
         return Code.SUCCESS;
     }
+    /*
+    creates a string of the list of books
+    @returns a string with the books in self and the amount of copies
+     */
     public String listBooks(){
         StringBuilder sb = new StringBuilder();
         String plural;
