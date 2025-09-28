42b2c50cc566ff3645d8ca87b94675e7eaa90224
jesscococ09 <jesstorrealba@gmail.com>
2025-09-28 10:29:02 -0700

    added Constant Fields

---
+++ src/main/java/Shelf.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+/**
+ * [Brief one-sentence description of what this class does.]
+ *
+ * @author Jessika Torrealba
+ * @version 0.1.0
+ * @since 9/28/2025
+ */
+public class Shelf {
+    public final int SHELF_NUMBER=0;
+    public final int SUBJECT_=1;
+
+    private final HashMap<Book, Integer> books;
+    private final int selfNumber;
+    private final String subject;
+
+
+
+
+}
